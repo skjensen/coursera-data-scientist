a34d1f67dbebfbe51f159acf8f47199f308b7a97
skjensen <skj@flixi.dk>
2016-06-05 21:16:17 +0100

    R Programming Week 2 assigment  Complete function added

--- R Programming/Week2/airpolution.R
+++ R Programming/Week2/airpolution.R
@@ -1,3 +1,21 @@
+complete <- function(directory, id = 1:332) {
+  
+  # Get the files names
+  file_list <- list.files(path=directory, pattern="*.csv", full.names=TRUE)
+  #print(file_list)
+  
+  # Create the data frame for filename and number of complete cases
+  data_frame <- data.frame(id=numeric(0), nobs= numeric(0))
+  # Read the CSV files into the data frame one file at the time. 
+  for(i in id){
+    #print(sum(complete.cases(read.csv(file_list[i],header=TRUE))))
+    data_frame <- rbind(data_frame, list(id = i, nobs = sum(complete.cases(read.csv(file_list[i],header=TRUE)))))
+  }
+  print(data_frame)
+  
+}
+
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   # Get the files names
@@ -10,7 +28,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   # mean vector
   m <- c()
   
-  # Read the CSV files inbto the data frame one file at the time. 
+  # Read the CSV files into the data frame one file at the time. 
   for(i in id){
     #print(dim(data_frame))  
     data_frame <- rbind(data_frame, read.csv(file_list[i],header=TRUE))
